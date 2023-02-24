d020d11f315a8e295156df14c5f8582d7bb65121
Matthew Baer <matt.baer77@gmail.com>
2023-02-23 23:27:17 -0500

    tag functionality added - tags on post pages still needed

--- app.py
+++ app.py
@@ -172,3 +172,76 @@ def edit_post_details_submit(post_id):
     db.session.commit()
 
     return render_template("post-detail.html", post=post)
+
+# TAG ROUTES
+
+@app.route('/tags')
+def list_tags():
+    """displays a list of tags"""
+    tags = Tag.query.all()
+
+    return render_template('tags.html', tags=tags)
+
+@app.route('/tags/<int:tag_id>')
+def tag_detail(tag_id):
+    """displays tag details"""
+
+    tag = Tag.query.get_or_404(tag_id)
+
+    return render_template('tag-detail.html', tag=tag)
+
+@app.route('/tags/new')
+def add_tag():
+    """displays form to add new tag"""
+
+    return render_template("add-tag.html")
+
+@app.route('/tags/new', methods=["POST"])
+def post_add_tag():
+    """displays form to add new tag"""
+
+    name = request.form["name"]
+
+    new_tag = Tag(name=name)
+
+    db.session.add(new_tag)
+
+    db.session.commit()
+
+    return redirect("/tags")
+
+@app.route('/tags/<int:tag_id>/edit')
+def edit_tag(tag_id):
+    """displays form to edit existing tag by id"""
+
+    tag = Tag.query.get_or_404(tag_id)
+
+    return render_template("edit-tag.html", tag=tag)
+
+@app.route('/tags/<int:tag_id>/edit', methods=["POST"])
+def post_edit_tag(tag_id):
+    """posts changes to a tag by id"""
+
+    tag = Tag.query.get_or_404(tag_id)
+
+    name = request.form["name"]
+
+    tag.name = name
+
+    db.session.add(tag)
+
+    db.session.commit()
+
+    return redirect("/tags")
+
+@app.route('/tags/<int:tag_id>/delete', methods=["POST"])
+def post_delete_tag(tag_id):
+    """deletes tag by id"""
+
+    tag = Tag.query.get_or_404(tag_id)
+
+    db.session.delete(tag)
+
+    db.session.commit()
+
+    return redirect('/tags')
